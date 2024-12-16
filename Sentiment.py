d41be56a5bf7e4b2049e40bf4325d655e6771654
myivtah <121180582+myivtah@users.noreply.github.com>
2024-12-16 20:12:40 +0700

    Update Sentiment.py

--- Sentiment.py
+++ Sentiment.py
@@ -3,28 +3,27 @@ import pandas as pd
 import re
 import string
 import nltk
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import os
 
-# Tentukan direktori untuk menyimpan data NLTK di Streamlit
-nltk_data_dir = '/home/appuser/nltk_data'
+# Tentukan lokasi direktori untuk NLTK di Streamlit
+nltk_data_dir = '/home/appuser/nltk_data'  # Pastikan ini sesuai dengan lingkungan Streamlit
 nltk.data.path.append(nltk_data_dir)
 
-# Fungsi untuk mendownload sumber daya jika belum ada
+# Fungsi untuk mengunduh sumber daya jika belum ada
 def download_if_not_exists(package):
     try:
         nltk.data.find(f'tokenizers/{package}')
         print(f"'{package}' sudah ada.")
     except LookupError:
+        print(f"'{package}' tidak ditemukan. Mengunduh...")
         nltk.download(package, download_dir=nltk_data_dir)
 
-# Download sumber daya yang diperlukan
+# Pastikan sumber daya yang diperlukan ada
 download_if_not_exists('punkt')
 download_if_not_exists('vader_lexicon')
 
 # Inisialisasi analisis sentimen VADER
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 sid = SentimentIntensityAnalyzer()
 
 # Fungsi preprocessing
@@ -37,7 +36,6 @@ def preprocess_text(kalimat):
 
 # Fungsi tokenisasi
 def tokenize_text(kalimat):
-    # Pastikan sumber daya NLTK telah terunduh sebelum tokenisasi
     try:
         return nltk.tokenize.word_tokenize(kalimat)
     except LookupError:
@@ -76,6 +74,8 @@ if uploaded_file is not None:
                 df['processed_text'] = df[selected_column].apply(preprocess_text)
                 df['tokens'] = df['processed_text'].apply(tokenize_text)
                 # Proses pembersihan token, stemming, dan penambahan label sentimen
+                from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+                from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
                 factory = StopWordRemoverFactory()
                 stopwords = factory.get_stop_words()
                 df['tokens_cleaned'] = df['tokens'].apply(lambda tokens: [token for token in tokens if token not in stopwords])
